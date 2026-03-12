35039503104513fbe94e04b23c16e2192c317d99
YuhAsz01 <juhaszabel0@gmail.com>
2026-03-12 21:02:46 +0100

    secure update

--- server.js
+++ server.js
@@ -9,12 +9,42 @@ const db = new sqlite3.Database("database.db");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.get("/data.html", requireLogin, (req,res)=>{
+res.sendFile(__dirname + "/public/data.html");
+});
+app.get("/admin.html", requireAdmin, (req,res)=>{
+res.sendFile(__dirname + "/public/admin.html");
+});
 
 app.use(session({
   secret: "titok",
   resave: false,
   saveUninitialized: true
 }));
+
+function requireLogin(req,res,next){
+
+if(!req.session.user){
+return res.redirect("/login.html");
+}
+
+next();
+
+}
+
+function requireAdmin(req,res,next){
+
+if(!req.session.user){
+return res.redirect("/login.html");
+}
+
+if(req.session.user.role !== "admin"){
+return res.redirect("/index.html");
+}
+
+next();
+
+}
 
 db.serialize(() => {
 
@@ -34,12 +64,12 @@ db.serialize(() => {
   // ADMIN FELHASZNÁLÓ
   db.run(`
     INSERT OR IGNORE INTO users (id, username, password, role)
-    VALUES (1, 'admin', 'admin', 'admin')
+    VALUES (1, 'admin', 'retek7253', 'admin')
   `);
 
   db.run(`
     INSERT OR IGNORE INTO users (id, username, password, role)
-    VALUES (2, 'user', 'user', 'user')
+    VALUES (2, 'user', 'hüllő', 'user')
   `);
 
   // 4 SZEMÉLY
@@ -78,7 +108,7 @@ app.post("/login",(req,res)=>{
       if(user.role==="admin"){
         res.redirect("/admin.html");
       }else{
-        res.redirect("/index.html");
+        res.redirect("/data.html");
       }
 
     }
@@ -86,23 +116,23 @@ app.post("/login",(req,res)=>{
 
 });
 
-app.get("/debts",(req,res)=>{
+app.get("/debts", requireLogin, (req,res)=>{
 
-  db.all("SELECT * FROM debts",(err,rows)=>{
-    res.json(rows);
-  })
+db.all("SELECT * FROM debts",(err,rows)=>{
+res.json(rows);
+});
 
 });
 
-app.post("/pay",(req,res)=>{
+app.post("/pay", requireAdmin,(req,res)=>{
 
-  const {id,amount}=req.body;
+const {id,amount}=req.body;
 
-  db.run(
-    "UPDATE debts SET amount = amount - ? WHERE id=?",
-    [amount,id],
-    ()=>res.redirect("/admin.html")
-  )
+db.run(
+"UPDATE debts SET amount = amount - ? WHERE id=?",
+[amount,id],
+()=>res.send("ok")
+);
 
 });
 
@@ -114,11 +144,19 @@ console.log("Havi tartozás növelés");
 
 db.run(`
 UPDATE debts
-SET amount = amount + 800
+SET amount = amount - 800
 `);
 
 });
 
 app.listen(PORT,()=>{
   console.log("Server fut");
+});
+
+app.get("/logout",(req,res)=>{
+
+req.session.destroy(()=>{
+res.redirect("/index.html");
+});
+
 });
